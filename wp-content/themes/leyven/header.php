1a39d575e5a9d2a4d39ab39b60ac95239bde526d
Jamie <james.cranston91@gmail.com>
2015-06-04 19:02:38 +1000

    Header updates
    started implementing materialize header

--- wp-content/themes/leyven/header.php
+++ wp-content/themes/leyven/header.php
@@ -13,7 +13,7 @@ if ( is_404() ) {
 		</style>
 		<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>" />
 		<title><?php bloginfo('name'); ?><?php if(is_home()) { bloginfo('description');} else { wp_title('|'); } ?></title>
-		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+		<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
 		<link rel="stylesheet" type="text/css" media="all" href="<?php echo get_bloginfo('template_directory'); ?>/materialize.min.css"/>
         <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
@@ -34,33 +34,25 @@ if ( is_404() ) {
 		  })();
 		</script>
         <script type="text/javascript" src="<?php echo get_bloginfo('template_directory');?>/js/materialize.min.js"></script>
+         <script type="text/javascript" src="<?php echo get_bloginfo('template_directory');?>/js/main.js"></script>
 	</head>
 	<body <?php body_class($class); ?>>
 		<div id="nonFooter">
 			<div id="wrapper">
-				<div id="header">
-					<div id="inside_header">
-						<div id="the_logo">
-							<div class="the_site_name">
-								<a href="<?php echo get_option('home'); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/top-logo.png" alt="logo" /></a>
-							</div>
-							<div id="the_site_title">
-								<div id="site-description">Relieving your financial burden<br />so that you can focus on your business</div>
-							</div>
-							<div class="the_site_right">
-								<div id="the_site_right_call">CALL: 0414 899 487</div>
-								<div id="the_site_right_image"><img src="<?php bloginfo('template_directory'); ?>/images/groups.jpg" alt="logo" /></div>
-							</div>
-						</div>
-					</div>
+				<div id="header row col s12">
+					  <a href="<?php echo get_option('home'); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/top-logo.png" alt="logo" /></a>
+						
+                <nav class="valign-wrapper">
+				<div id="the_nav_line valign">
+					
+						<div class="inside_nav nav-wrapper">
+							<?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'menu_class' => 'nav center hide-on-med-and-down', 'theme_location' => 'primary-menu' ) ); ?>
+						<?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'menu_class' => 'side-nav', 'menu_id' => 'slide-out', 'theme_location' => 'primary-menu' ) ); ?>
+                    </div>
+                    <a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
 				</div>
-				<div class="the_clear"></div>
-				<div id="the_nav_line">
-					<div class="the_nav">
-						<div class="inside_nav">
-							<?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'menu_class' => 'nav', 'theme_location' => 'primary-menu' ) ); ?>
-						</div>
-					</div>
-				</div>
+                    
+                </nav>
+                    </div>
 				<div class="the_clear"></div>
 				<div id="the_mid">
