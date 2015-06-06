64b3715e902f60ef1771dbccafd191915c1d0534
Jamie <james.cranston91@gmail.com>
2015-06-06 15:42:33 +1000

    Slider scaling added
    CSS media queries to support scaling of slider down to mobile

--- wp-content/themes/leyven/slides/home/slides-home.php
+++ wp-content/themes/leyven/slides/home/slides-home.php
@@ -1,6 +1,6 @@
 <ul class="slides">
                   <li>
-                    <img id="home" class="one" src="wp-content/themes/leyven/images/home-Slide-1.jpg"> <!-- random image -->
+                    <img id="home" class="one responsive-img" src="wp-content/themes/leyven/images/home-Slide-1.jpg"> <!-- random image -->
                     <div class="caption card custom-positioned-caption">
                       <h5 id="home" class="orange-text col s12 m6 l6 left">Welcome to Leyven Bookkeeping</h5><h6 id="home" class="orange-text col s12 m6 l6 left thin"> Complete Bookkeeping and BAS Services</h6>
                     </div>
