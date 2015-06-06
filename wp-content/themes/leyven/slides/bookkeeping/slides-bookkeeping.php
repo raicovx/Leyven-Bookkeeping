8179c3676742614c8565e3f4fa37919835473de9
Jamie <james.cranston91@gmail.com>
2015-06-06 14:49:45 +1000

    Implemented Slider on Home and Bookkeeping pages
    Added slider to home and bookkeeping pages, also added custom captions
    and most of the images are very lightly colour/lit and therefore i
    thought the phrases needed some differentiation. The text is now
    actually text which will be picked up by google in crawling (rather than
    embedded in an image where it is not noticed.). Took the extra time to
    fix the typography on the single slides at present.

---
+++ wp-content/themes/leyven/slides/bookkeeping/slides-bookkeeping.php
@@ -0,0 +1,8 @@
+<ul class="slides">
+                  <li>
+                    <img id="bookkeeping" class="one" src="/Leyven-Bookkeeping/wp-content/themes/leyven/images/bookkeeping-Slide-1.jpg"> <!-- random image -->
+                    <div class="caption card custom-positioned-caption">
+                      <h5 id="bookkeeping" class="orange-text col s12 m6 l6 left">Over 20 years bookkeeping experience, helping western sydney business owners like you!</h5>
+                    </div>
+                  </li>
+                </ul>
